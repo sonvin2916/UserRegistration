e43775367209248312164759cde8e00dd2d517a3
sonvin2916 <sonalchougule07@gmail.com>
2020-06-25 12:10:31 +0530

    UC-1 : Added

--- userReg.sh
+++ userReg.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
-
+shopt -s extglob
 echo "Welcome to user registration"
+
+function name()
+{
+
+	pattern="^[[:upper:]]+([a-z]{3,})"
+	if [[ $fname =~ $pattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+}
+read -p " Enter first name : " fname
+name $fname
